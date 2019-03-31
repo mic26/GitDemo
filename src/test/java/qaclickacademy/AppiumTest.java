1e9fe6574fff275070664fbd010ccfd7fcf9c2b7
mohnish <pradhanmohnish@gmail.com>
2019-03-31 14:59:30 +0530

    First commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void AppAndroid()
+	{
+		System.out.println("AppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("ISOApp");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		System.out.println("browse");
+	}
+	
+	@Test
+	public void uiautomation()
+	{
+		System.out.println("uiautomation");
+	}
+}
